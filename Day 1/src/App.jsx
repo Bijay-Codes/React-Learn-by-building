d7a9ae86ef752766b3d57caec75c4255100bf848
Bijay-Codes <bijayprojects@gmail.com>
2026-04-13 07:04:03 +0530

    Fixed the project on day 2 Finally i can rest in peace

--- Day 1/src/App.jsx
+++ Day 1/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react'
 import './app.css';
 export default App
-
+/*
 function App() {
   const [mood, setMood] = useState('Normal');
   let count = useRef(0);
@@ -18,6 +18,7 @@ function App() {
     </div>
   )
 }
+*/
 const moodData = [
   {
     mood: 'Happy',
@@ -36,6 +37,7 @@ const moodData = [
     emoji: '🤗'
   }
 ]
+/*
 function handleCycle(setMood, count, id) {
   id.current = setInterval(() => {
     if (count.current >= moodData.length) {
@@ -44,4 +46,35 @@ function handleCycle(setMood, count, id) {
     setMood(moodData[count.current].mood)
     count.current++;
   }, 3000);
+}
+*/
+function App() {
+  const [mood, setMood] = useState('Normal 🙂');
+  let i = useRef(0);
+
+  return (
+    <div className='mood-proj'>
+      <div className={`mood-card ${sliceEmoji(mood)}`}>{mood}</div>
+      {/* <button className={`mood-button ${mood}`} onClick={() => handleClick(i, setMood)}>Click me</button> */}
+      <Button callback={() => handleClick(i, setMood)} mood={mood} />
+    </div>
+  )
+}
+function Button({ callback, mood }) {
+  return (
+    <button className={`mood-button ${sliceEmoji(mood)}`} onClick={callback}>{!mood ? 'Click me' : mood}</button>
+  )
+}
+function handleClick(i, setMood) {
+  if (i.current >= moodData.length) {
+    i.current = 0;
+  }
+  console.log(i.current)
+  setMood(moodData[i.current].mood + ' ' + moodData[i.current].emoji);
+  i.current++;
+}
+
+function sliceEmoji(input) {
+  console.log(input.slice(0, input.length - 2))
+  return input.slice(0, input.length - 2);
 }
